0072aea1700db21d55f645257f52caf29621a209
bohdanrodz <bohdanrodz@gmail.com>
2025-08-13 17:14:34 +0200

    hw: JAVASCRIPT HOMEWORK 1 TASKS 1-4 COMPLETE

---
+++ javascript/Homeworks/Homework1/Task 4/main.js
@@ -0,0 +1,5 @@
+name = prompt("Enter your name");
+middleName = prompt("Enter your middle name");
+age = prompt("Enter your age");
+
+console.log(name, middleName, age);
---
+++ javascript/Homeworks/Homework1/Task2/main.js
@@ -0,0 +1,9 @@
+let firstName = 'Bohdan';
+let middleName = 'Tarasovych';
+let lastName = 'Rodz';
+
+let person;
+person = firstName + ' ' + middleName + ' ' + lastName;
+// person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person);
